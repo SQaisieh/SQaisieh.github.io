7961e23e5f89b5c7573b81e18fc5738ab3fc4b80
SQaisieh <skq0204663@lpssonline.com>
2024-02-15 13:56:21 +0000

    saving data shapes

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -43,12 +43,29 @@ $(document).ready(function () {
     shape: "circle",
     repeat: 3
   };
-
+  dataShapes.push(shape);
   // TODO 2: add a new property to all data shapes
-  
+  for (var i = 0 ; i < dataShapes; i++){
+    var currentShape = (dataShapes.color[i])
+
+    if (currentShape === "red"){
+      currentShape.goodBehavior = "bounce"
+    }
+    else if (currentShape === "blue"){
+      currentShape.goodBehavior = "blink"
+    }
+    else{
+      currentShape.goodBehavior = "spin"
+    }
+
+  }
+
 
   // TODO 3-a: add a function that handles the static display type
-  
+  function handleStatic(data){
+    setBackgroundWithObject(data)
+    animationDetails.displayType = 1
+  }
 
   // TODO 4-a: add a function that handles the good display type
   
@@ -62,7 +79,7 @@ $(document).ready(function () {
 
   function staticDisplay() {
     // TODO 3-b: call your handleStatic function
-    
+    handleStatic(dataShapes[currentIndex])
   }
 
   function goodDisplay() {
